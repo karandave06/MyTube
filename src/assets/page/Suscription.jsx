a2eee06522bb8676bcdffca9f0e3a1b4f2843420
bhargav <karandave503@gmail.com>
2024-01-17 19:09:55 +0530

    add suscription option

--- src/assets/page/Suscription.jsx
+++ src/assets/page/Suscription.jsx
@@ -1,12 +1,41 @@
-import  Card  from '../../components/Card'
-import React from 'react'
+import axios from "axios";
+import Card from "../../components/Card";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import noVideo from "../../../public/images/novideo.png";
 
 const Suscription = () => {
-  return (
-    <div className="md:w-[calc(100%-4rem)] w-full grid  py-5  md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 p-4 ml-auto  h-full">
-    <Card />
-  </div>
-  )
-}
+  const [video, setVideo] = useState([]);
+  const current = useSelector((state) => state.user.current);
 
-export default Suscription
+  console.log(current?.other?._id);
+  useEffect(() => {
+    axios
+      .post(`${import.meta.env.VITE_SOME_KEY}/video/sub`, {
+        body: current?.other?._id,
+      })
+      .then((res) => {
+        setVideo(res.data);
+      });
+  }, []);
+console.log(video)
+  return (
+    <>
+      {video > 0 ? (
+        <div className="md:w-[calc(100%-4rem)] w-full grid  py-5  md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 p-4 ml-auto  h-full">
+          {video &&
+            video.map((res) => <Card key={res._id} video={res} id={res._id} />)}
+        </div>
+      ) : (
+        <div className="w-full h-full flex items-center justify-center flex-col">
+          <div className="md:w-[30rem] md:h-[25rem] w-[15rem] h-[15rem]">
+            <img src={noVideo} className="w-full h-full object-cover" alt="" />
+          </div>
+          <h1 className="md:text-2xl text-md font-bold">Please Suscribe..</h1>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Suscription;
